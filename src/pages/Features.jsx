452b1a82cdd4f257543c55640a066b66401e2e53
Cayden <caydenyoo@hotmail.com>
2025-10-19 12:41:18 -0700

    Added content to features page

--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -5,8 +5,27 @@ function Features() {
   return (
     <>
       <section className="features" id="features">
-        <h1 className="features__header">Features.</h1>
-        <h2 className="features__subheader">Coming soon...</h2>
+        <h1>Features.</h1>
+        <p>Tap on a feature to learn more.</p>
+        <img className="features__image" src="./mock_features.png" />
+        <div className="features__list">
+          <div className="features__list-item">
+            <h2>1. Feature</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris </p>
+          </div>
+          <div className="features__list-item">
+            <h2>2. Feature</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris </p>
+          </div>
+          <div className="features__list-item">
+            <h2>3. Feature</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris </p>
+          </div>
+          <div className="features__list-item">
+            <h2>4. Feature</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris </p>
+          </div>
+        </div>
       </section>
     </>
   );
